c0d73e47d0ed630ebf4dd4e65bbb4e2ced377979
Rune Botten <rbotten@gmail.com>
2015-11-25 12:40:20 +0100

    Flesh out some tests

--- api/v1.js
+++ api/v1.js
@@ -1,19 +1,21 @@
 import express from 'express'
 /*
 import cookieParser from 'cookie-parser'
-import bodyParser from 'body-parser'
 */
+import bodyParser from 'body-parser'
 
 const router = express.Router()
-
-/*
 router.use(bodyParser.json())
+/*
 router.use(bodyParser.urlencoded({extended: false}))
 router.use(cookieParser())
 */
 
-router.get('/events', (req, res, next) => {
-  res.json([])
+router.post('/events/:uid', (req, res, next) => {
+  res
+  .header('Location', `${req.originalUrl}/todo`)
+  .status(201)
+  .json('Created')
 })
 
 export default router
--- test/api/v1_tests.js
+++ test/api/v1_tests.js
@@ -5,12 +5,42 @@ import apiV1 from '../../api/v1'
 const app = express()
 app.use('/', apiV1)
 
-describe('GET /events', () => {
-  it('returns events', done => {
+const uid = 'post.entry:bengler.www$123'
+const event = {
+  name: 'upvote',
+  document: {
+    timeOffset: 120
+  }
+}
+
+describe('POST /events/:uid', () => {
+  it('returns 201 Created', done => {
     request(app)
-    .get('/events')
-    .expect('Content-Type', 'application/json; charset=utf-8')
-    .expect([])
-    .expect(200, done)
+    .post(`/events/${uid}`)
+    .send(event)
+    .expect(201, done)
   })
+
+  it('has relative path for created resource in Location header', done => {
+    request(app)
+    .post(`/events/${uid}`)
+    .send(event)
+    .expect('Location', /events\/\d+/, done)
+  })
+
+  it('creates a new event for :uid', done => {
+    request(app)
+    .post(`/events/${uid}`)
+    .send(event)
+    .end((error, response) => {
+      if (error) {
+        return done(error)
+      }
+      // Do assertions on the result
+      // const result = response.body
+      done()
+    })
+  })
+
+  it('requires an identity')
 })
