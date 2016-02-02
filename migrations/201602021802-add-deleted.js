8625825390ee954d7424dbc4efae3e2dd82e7046
Rune Botten <rbotten@gmail.com>
2016-02-02 18:08:38 +0100

    add deleted column for Events

---
+++ migrations/201602021802-add-deleted.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.addColumn('Events', 'deleted', Sequelize.BOOLEAN);
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.removeColumn('Events', 'deleted');
+  }
+};
