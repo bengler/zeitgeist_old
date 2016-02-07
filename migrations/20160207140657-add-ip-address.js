64af3b6599526be28f2edabd576d9d4a272bd367
Rune Botten <rbotten@gmail.com>
2016-02-07 14:55:50 +0100

    Forgot to include new migration

---
+++ migrations/20160207140657-add-ip-address.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.addColumn(
+      'Events',
+      'clientIp',
+      {
+        type: Sequelize.STRING,
+        allowNull: true
+      }
+    )
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.removeColumn('Events', 'clientIp')
+  }
+};
